1e706d2314609586e0a8bab393425f38977d1562
Mitch Pierias <mitch@pierias.com>
2018-11-27 12:00:39 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# EOS Milestone Initial Security Offering
+
+> A sample escrow contract responsible for managing share value and fund release based on project milestones, governed by the current share holders.
+
+The Milestone ICO is designed to supplement payment to developers for work completed, while protecting investors from projects with no intent.
+
+### Investor Registration
+For this we are not going to allow accounts to preregister. The reason being is that our ICO contract is responsible for covering the funds for wallet storage, so we will be required to withhold the RAM for accounts who have not purchased any tokens.
---
+++ miso.hpp
@@ -0,0 +1,51 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/singleton.hpp>
+#include <eosiolib/print.h>
+#include <eosiolib/transaction.hpp>
+#include <string>
+
+using namespace eosio;
+using eosio::asset;
+using std::string;
+
+class miso : public eosio::contract {
+	
+
+	public:
+		// Constructor
+		explicit miso(action_name self) : contract(self), config(_self, _self) {
+
+		}
+		// @abi action
+		void setowner(const name delegate);
+		// @abi action
+		void purchase(const account_name account, const asset amount, const asset paid);
+		// @abi action
+		void witness(const account_name account, const uint64_t task_id);
+
+	private:
+		// @abi table wallets i64
+		struct Wallet {
+			account_name	account;
+			uint64_t		balance;
+
+			auto primary_key() const {
+				return account;
+			};
+
+			EOSLIB_SERIALIZE(Wallet, (account)(balance));
+		};
+		// @abi table tasks i64
+		struct Task {
+			uint64_t		id;
+			string			name;
+		}
+
+		struct Config {
+			account_name		owner;
+		};
+
+		typedef singleton<N(config), Config> config_table;
+
+		config_table config;
+};
